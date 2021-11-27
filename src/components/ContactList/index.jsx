a9b3396f995d87e5a75cae4d0c0d581b5f52da6e
Rahul Parmar <rahulparmar.n@gmail.com>
2021-11-27 09:18:58 +0530

    added contact list data

--- src/components/ContactList/index.jsx
+++ src/components/ContactList/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
@@ -6,12 +6,19 @@ import ListItemText from '@mui/material/ListItemText'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import Checkbox from '@mui/material/Checkbox'
 import Avatar from '@mui/material/Avatar'
+import { styled } from '@mui/material/styles'
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
+import UserDetailsCard from '../UserDetailsCard'
+import users from '../../__mock__/users.json'
 
 const ContactList = () => {
   const [checked, setChecked] = React.useState([1])
-
+  const [currentUser, setCurrentUser] = useState(null)
   const handleToggle = (value) => () => {
     const currentIndex = checked.indexOf(value)
+
     const newChecked = [...checked]
 
     if (currentIndex === -1) {
@@ -24,33 +31,45 @@ const ContactList = () => {
   }
 
   return (
-    <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {[0, 1, 2, 3].map((value) => {
-        const labelId = `checkbox-list-secondary-label-${value}`
-        return (
-          <ListItem
-            key={value}
-            disablePadding
-          >
-            <ListItemButton>
-              <Checkbox
-                edge="end"
-                onChange={handleToggle(value)}
-                checked={checked.indexOf(value) !== -1}
-                inputProps={{ 'aria-labelledby': labelId }}
-              />
-              <ListItemAvatar>
-                <Avatar
-                  alt={`Avatar n°${value + 1}`}
-                  src={`/static/images/avatar/${value + 1}.jpg`}
-                />
-              </ListItemAvatar>
-              <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-            </ListItemButton>
-          </ListItem>
-        )
-      })}
-    </List>
+    <>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+          <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {users.map((user) => {
+              const labelId = `checkbox-list-secondary-label-${user.username}`
+              return (
+                <ListItem
+                  key={user.username}
+                  disablePadding
+                  onClick={handleToggle(user.username)}
+                >
+                  <ListItemButton>
+                    <Checkbox
+                      edge="end"
+                      onChange={handleToggle(user.username)}
+                      checked={checked.indexOf(user.username) !== -1}
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                    <ListItemAvatar>
+                      <Avatar
+                        alt={user.username}
+                        src={user.username}
+                      />
+                    </ListItemAvatar>
+                    <ListItemText id={labelId} primary={user.username} />
+                    <ListItemText primary={user.company.name} />
+                  </ListItemButton>
+                </ListItem>
+              )
+            })}
+          </List>
+        </Grid>
+        <Grid item xs={6}>
+          <UserDetailsCard />
+        </Grid>
+      </Grid>
+
+    </>
   )
 }
 
--- src/components/UserDetailsCard/index.jsx
+++ src/components/UserDetailsCard/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const UserDetailsCard = ({ user }) => {
     return (
         <div>
-
+            test
         </div>
     )
 }
